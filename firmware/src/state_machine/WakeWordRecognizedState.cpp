a306f19735f3c318baa56788f49456c964b3272c
ensingerphilipp <philippensi2@gmail.com>
2021-12-29 15:02:05 +0100

    Commit Local Changes - initial ESP32 Firmware with WakeWord-Detection

---
+++ firmware/src/state_machine/WakeWordRecognizedState.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "I2SSampler.h"
+#include "RingBuffer.h"
+#include "WakeWordRecognizedState.h"
+#include "IndicatorLight.h"
+#include "../config.h"
+#include <string.h>
+
+WakeWordRecognizedState::WakeWordRecognizedState(I2SSampler *sample_provider, IndicatorLight *indicator_light )
+{
+    // save the sample provider for use later
+    m_sample_provider = sample_provider;
+    m_indicator_light = indicator_light;
+
+}
+void WakeWordRecognizedState::enterState()
+{
+    return;
+}
+
+bool WakeWordRecognizedState::run()
+{
+    return true;
+}
+
+void WakeWordRecognizedState::exitState()
+{
+    uint32_t free_ram = esp_get_free_heap_size();
+    Serial.printf("Free ram after request %d\n", free_ram);
+}
